90026dd1d87f002216fa089abf0c62205850e5c9
SarwatMustafa-03 <sarwatmustafa03@gmail.com>
2025-08-18 10:01:38 -0700

    scent n stories

---
+++ src/components/Header/card.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const Cards = () => {
+    return (
+        <>
+            <div>
+                <h3>Executive Range</h3>
+                <h4>View all</h4>
+            </div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure quas illo itaque hic ut quaerat. Earum quibusdam iure quasi deleniti iusto enim ut, qui nesciunt quam odio, atque, laborum cum.
+
+            <div>
+                <div><img src="./images/image1.webp" alt="perfume img" />
+                    <h3>Catch 22</h3>
+                    <h4>Best Seller</h4>
+                    <h5>From Rs.1900</h5>
+                </div>
+
+                <div><img src="./images/image1.webp" alt="perfume img" />
+                    <h3>Catch 22 for</h3>
+                    <h4>Best Seller</h4>
+                    <h5>From Rs.1900</h5>
+                </div>
+
+                <div><img src="./images/image1.webp" alt="perfume img" />
+                    <h3>Catch 22</h3>
+                    <h4>Best Seller</h4>
+                    <h5>From Rs.1900</h5>
+                </div>
+
+                <div><img src="./images/image1.webp" alt="perfume img" />
+                    <h3>Catch 22</h3>
+                    <h4>Best Seller</h4>
+                    <h5>From Rs.1900</h5>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Cards;
