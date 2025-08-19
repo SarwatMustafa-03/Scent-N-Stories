22d255cb54767b2104eafdfbcf41d8eff0f3370c
SarwatMustafa-03 <sarwatmustafa03@gmail.com>
2025-08-19 10:34:50 -0700

    initial web

--- src/components/Header/card.jsx
+++ src/components/Header/card.jsx
@@ -15,7 +15,7 @@ const Cards = () => {
                     <h4>Best Seller</h4>
                     <h5>From Rs.1900</h5>
                 </div>
-
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui, pariatur? Id blanditiis officia aliquam incidunt. Sit, omnis? Voluptas ratione vitae rerum earum sint eveniet dicta veritatis, perspiciatis exercitationem, hic vero.</p>
                 <div><img src="./images/image1.webp" alt="perfume img" />
                     <h3>Catch 22 for</h3>
                     <h4>Best Seller</h4>
