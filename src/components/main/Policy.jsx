23245494f474321d2e2be0b7248bcfed63b09fc5
SarwatMustafa-03 <sarwatmustafa03@gmail.com>
2025-08-29 04:50:00 -0700

    Scent N  Stories

---
+++ src/components/main/Policy.jsx
@@ -0,0 +1,15 @@
+import { MdPolicy } from "react-icons/md";
+import { FaAward } from "react-icons/fa6";
+import { FaThumbsUp } from "react-icons/fa6";
+
+const Policy = () => {
+  return (
+    <div>
+      <MdPolicy />
+      <FaAward />
+      <FaThumbsUp />
+    </div>
+  )
+}
+
+export default Policy
