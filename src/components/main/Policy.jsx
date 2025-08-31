f67aee922b1794f580ff0acdc21de46922194032
SarwatMustafa-03 <sarwatmustafa03@gmail.com>
2025-08-31 14:28:58 +0500

    policy.jsx

--- src/components/main/Policy.jsx
+++ src/components/main/Policy.jsx
@@ -5,7 +5,7 @@ import { FaThumbsUp } from "react-icons/fa6";
 const Policy = () => {
   return (
     <div>
-      <MdPolicy />
+      <MdPolicy /> <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique rem consectetur temporibus reiciendis quam alias laborum vitae? Qui, quidem modi dolore corporis et reiciendis nam soluta quisquam culpa odio distinctio.</p>
       <FaAward />
       <FaThumbsUp />
     </div>
