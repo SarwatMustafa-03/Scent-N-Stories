b672d9008d6f73cdc018fec1d64e12a92af0ddc1
SarwatMustafa-03 <sarwatmustafa03@gmail.com>
2025-09-01 16:08:56 +0500

    policy.jsx

--- src/components/main/Policy.jsx
+++ src/components/main/Policy.jsx
@@ -6,8 +6,8 @@ const Policy = () => {
   return (
     <div>
       <MdPolicy /> <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique rem consectetur temporibus reiciendis quam alias laborum vitae? Qui, quidem modi dolore corporis et reiciendis nam soluta quisquam culpa odio distinctio.</p>
-      <FaAward />
-      <FaThumbsUp />
+      <FaAward /><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum nisi inventore perspiciatis quibusdam veritatis, modi odio at, delectus excepturi laudantium porro temporibus nesciunt vel corporis quia repellat impedit maxime facere.</p>
+      <FaThumbsUp /><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore odio, fuga impedit incidunt ipsa harum quam repellat aliquam perspiciatis molestiae, delectus dolore. Autem quidem accusamus quae dicta accusantium eum possimus.</p>
     </div>
   )
 }
