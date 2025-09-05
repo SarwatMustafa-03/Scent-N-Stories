aa784fcedb193ccaafcb94ac77a89f102e103a59
SarwatMustafa-03 <sarwatmustafa03@gmail.com>
2025-09-05 20:59:49 +0500

    policy

--- src/components/main/Policy.jsx
+++ src/components/main/Policy.jsx
@@ -7,6 +7,8 @@ const Policy = () => {
     <div>
       <MdPolicy  className="size "/> <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique rem consectetur temporibus reiciendis quam alias laborum vitae? Qui, quidem modi dolore corporis et reiciendis nam soluta quisquam culpa odio distinctio.</p>
       <FaAward /><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum nisi inventore perspiciatis quibusdam veritatis, modi odio at, delectus excepturi laudantium porro temporibus nesciunt vel corporis quia repellat impedit maxime facere.</p>
+     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo, laborum.</p>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, consequatur.</p>
       <FaThumbsUp /><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore odio, fuga impedit incidunt ipsa harum quam repellat aliquam perspiciatis molestiae, delectus dolore. Autem quidem accusamus quae dicta accusantium eum possimus.</p>
     </div>
   )
