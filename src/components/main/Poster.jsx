b440d485f7dfc5985b198640a41a5b1cb2062761
SarwatMustafa-03 <sarwatmustafa03@gmail.com>
2025-09-10 15:55:13 +0500

    poster

--- src/components/main/Poster.jsx
+++ src/components/main/Poster.jsx
@@ -2,9 +2,10 @@
 
 const Poster = () => {
   return (
-    <div>
-      <img src="https://scentsnstories.pk/cdn/shop/files/Kiosk_SNS.webp?v=1755845099&width=1200" alt="Poster" />
-    </div>
+    <span className="flex col justify-center overflow-x-hidden">
+    <img className=" rounded-2xl" src="https://scentsnstories.pk/cdn/shop/files/Tariq_Rd_Outlet.webp?v=1755856247&width=1200" alt="" />
+      <img className="rounded-2xl" src="https://scentsnstories.pk/cdn/shop/files/Kiosk_SNS.webp?v=1755845099&width=1200" alt="Poster" />
+    </span>
   )
 }
 
